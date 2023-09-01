e1a7ce3ce6f86b1d4ea59bcdcfc968d2a8029796
seamus52 <7493527+seamus52@users.noreply.github.com>
2023-09-01 06:37:17 -0700

    stage 3 complete

--- Password Hacker (Python)/task/hacking/hack.py
+++ Password Hacker (Python)/task/hacking/hack.py
@@ -7,6 +7,13 @@ from string import ascii_lowercase, digits
 # constant parameters
 MAX_PWD_LEN = 20
 RECV_SIZE = 1024
+PASSWORD_FILE = "passwords.txt"
+
+
+def read_password_file():
+    with open(PASSWORD_FILE) as f:
+        for line in f:
+            yield line.strip()
 
 
 def brute_force_generator():
@@ -17,6 +24,14 @@ def brute_force_generator():
                 continue
 
             yield "".join(passwordCandidate)
+
+
+def dict_based_generator():
+    for raw_password in read_password_file():
+        mixed_case_combinations_of_pwd = list(map("".join,
+                                                  itertools.product(*zip(raw_password.upper(), raw_password.lower()))))
+        for password_candidate in list(mixed_case_combinations_of_pwd):
+            yield password_candidate
 
 
 def password_cracker(hostname, port, password_generator):
@@ -46,4 +61,5 @@ args = sys.argv
 hostname = args[1]
 port = int(args[2])
 
-password_cracker(hostname, port, brute_force_generator())
+# password_cracker(hostname, port, brute_force_generator())
+password_cracker(hostname, port, dict_based_generator())
